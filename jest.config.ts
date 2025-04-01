4749c27cb38504d4f694921d75f5d1a4bc6f9add
vjcajilemac <vjcajilema@gmail.com>
2025-04-01 16:20:10 -0500

    docker

--- jest.config.ts
+++ jest.config.ts
@@ -1,13 +1,9 @@
-import { pathsToModuleNameMapper } from 'ts-jest';
-import { compilerOptions } from './tsconfig.json';
-
 export default {
-  preset: 'ts-jest',
-  testEnvironment: 'jsdom',
-  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths || {}, { prefix: '<rootDir>/' }),
-  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
-  testMatch: ['<rootDir>/src/tests/**/*.test.tsx', '<rootDir>/src/tests/**/*.test.ts'],
-  transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest',
+  testEnvironment: "jest-environment-jsdom", // Same name of the lib you installed
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"], // The file you created to extend jest config and "implement" the jest-dom environment in the jest globals
+  moduleNameMapper: {
+    "\\.(gif|ttf|eot|svg|png)$": "<rootDir>/test/__mocks__/fileMock.js", // The global stub for weird files
+    "\\.(css|less|sass|scss)$": "identity-obj-proxy", // The mock for style related files
+    "^@/(.*)$": "<rootDir>/src/$1", // [optional] Are you using aliases?
   },
 };
